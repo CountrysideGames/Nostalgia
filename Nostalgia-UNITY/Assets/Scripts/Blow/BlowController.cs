8cf95b8bd7d3a012a881111caf9d03511682af24
MarceloBarce <marcelobarce@live.com>
2014-10-22 20:01:20 -0200

    Mini-game Blow (cartucho)
    Sujeira na fita

--- Nostalgia-UNITY/Assets/Scripts/Blow/BlowController.cs
+++ Nostalgia-UNITY/Assets/Scripts/Blow/BlowController.cs
@@ -3,23 +3,55 @@ using System.Collections;
 
 public class BlowController : MonoBehaviour {
 
+	public SpriteRenderer dirtRenderer; //sprite da sujeira do cartucho
+	public Sprite[] dirtSprites = new Sprite[6]; //sprites
+
+	private int state = 0; //state 0 = incompleto / state 1 = completo
 
 	private MicControlC micControl;
-	private SpriteRenderer sprite;
+	private SpriteRenderer windRenderer;
+	private float progress = 0;
 
 
 	void Awake ()
 	{
 		micControl = GetComponent<MicControlC>();
-		sprite = GetComponent<SpriteRenderer>();
+		windRenderer = GetComponent<SpriteRenderer>();
 	}
 
 
 	void Update ()
 	{
-		if (micControl.loudness > 20)
-			sprite.enabled = true;
-		else
-			sprite.enabled = false;
+		if (state == 0)
+		{
+			if (micControl.loudness > 40)
+			{
+				windRenderer.enabled = true;
+				progress += 1;
+			}
+			else
+			{
+				windRenderer.enabled = false;
+			}
+			
+			if (progress > 25)
+				dirtRenderer.sprite = dirtSprites [1];
+			if (progress > 50)
+				dirtRenderer.sprite = dirtSprites [2];
+			if (progress > 75)
+				dirtRenderer.sprite = dirtSprites [3];
+			if (progress > 100)
+				dirtRenderer.sprite = dirtSprites [4];
+			if (progress > 125)
+			{
+				dirtRenderer.sprite = dirtSprites [5];
+				state = 1;
+			}
+		}
+		else if (state == 1)
+		{
+			Game.EndLevel (true);
+			state = 3;
+		}
 	}
 }
